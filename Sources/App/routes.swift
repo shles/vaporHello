d11455327b81f9be57d09d8aa4e624f3075121b3
Artemiy <temitrix@gmail.com>
2018-07-10 14:53:08 -0400

    first commit

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -4,7 +4,7 @@ import Vapor
 public func routes(_ router: Router) throws {
     // Basic "Hello, world!" example
     router.get("hello") { req in
-        return "Hello, world!"
+        return "Hello, Shles!"
     }
 
     // Example of configuring a controller
